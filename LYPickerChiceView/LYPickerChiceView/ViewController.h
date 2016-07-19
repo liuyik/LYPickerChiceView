15bbf36ac3132b3a36c1a7a06f9dd30f5370bf6f
刘毅 <352601605@qq.com>
2016-07-19 17:56:35 +0800

    创建

---
+++ LYPickerChiceView/LYPickerChiceView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYPickerChiceView
+//
+//  Created by 刘毅 on 16/7/19.
+//  Copyright © 2016年 刘毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYPickerChiceView/LYPickerChiceView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LYPickerChiceView
+//
+//  Created by 刘毅 on 16/7/19.
+//  Copyright © 2016年 刘毅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
